0734ed0af6c23e734da665b6876f0de7afc0e9b9
eleanordavis <93998723+eleanordavis@users.noreply.github.com>
2021-11-09 11:00:42 -0500

    Delete keeping_record.py

--- keeping_record.py
+++
@@ -1,4 +0,0 @@
-Python 3.9.6 (v3.9.6:db3ff76da1, Jun 28 2021, 11:14:58) 
-[Clang 12.0.5 (clang-1205.0.22.9)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
